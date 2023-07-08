6f5ab2e51bc47a9caf5f422b254cc4897924ee64
D_arh <dimonarhipon@mail.ru>
2023-07-08 23:45:16 +0300

    feat добавляет текст

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 			<div className='card'>
 				<button onClick={buttonClickHandler}>count is {count}</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Edit <code>src/App.tsx</code> and save to test HMR nd save to test HMR
 				</p>
 			</div>
 			<p className='read-the-docs'>
