e97534c1b73351db9b88f837ecf5a76e5ec6c312
D_arh <dimonarhipon@mail.ru>
2023-07-08 23:24:14 +0300

    feat добавляет текст

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ function App() {
 			</div>
 			<p className='read-the-docs'>
 				Click on the Vite and React logos to learn more Click on the Vite and React logos to learn
+				more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn
 				more Click on the Vite and React logos to learn more
 			</p>
 		</>
