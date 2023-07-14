60ec0e028b6158f64dcc9aa890749116098ea819
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-07-15 01:24:55 +0300

    feat: добавлена функция показать пароль

--- src/widgets/Input/Input.tsx
+++ src/widgets/Input/Input.tsx
@@ -2,12 +2,21 @@ import { useState } from 'react';
 import styles from './Input.module.scss';
 import { IInput } from '../../shared/Types/AuthTypes';
 
-const Input: React.FC<IInput> = ({ label, errorMessage, onChange, ...inputProps }) => {
+const Input: React.FC<IInput> = ({ label, errorMessage, onChange, type, ...inputProps }) => {
 
+    const imgPath= {
+        eye: '/eye.svg',
+        eyeSlash: '/eyeslash.svg',
+    };
+    const [typeInput, setTypeInput] = useState(type);
     const [focused, setFocused] = useState(false);
 
-    const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    const onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
         setFocused(true);
+    };
+
+    const hidePassword = () => {
+        typeInput === 'password' ? setTypeInput('text') : setTypeInput('password');
     };
 
     return (
@@ -17,13 +26,15 @@ const Input: React.FC<IInput> = ({ label, errorMessage, onChange, ...inputProps 
                 className={styles.input}
                 {...inputProps}
                 onChange={onChange}
-                onBlur={handleFocus}
+                onBlur={onFocus}
+                type={typeInput}
                 onFocus={() =>
                     inputProps.name === 'confirmPassword' && setFocused(true)
                 }
                 focused={focused.toString()}
             />
             <span className={styles.span}>{errorMessage}</span>
+            {type === 'password' && <div onClick={hidePassword} className={styles.passwordHide}><img src={typeInput === 'password' ? imgPath.eye : imgPath.eyeSlash} alt='eyePassword' /></div>}
         </div>
     );
 };
