19f5932cea22cc76db3d640092c97f1fc2f7d44b
D_arh <dimonarhipon@mail.ru>
2023-07-14 20:19:11 +0300

    fix: исправляет warningn in eslint

--- src/shared/api/callApi.ts
+++ src/shared/api/callApi.ts
@@ -2,8 +2,7 @@ import NoteType from '../lib/NoteType';
 
 // TASK доделать функцию
 export const callApi = async (url: string): Promise<NoteType[]> => {
-	const data: any = await fetch(url);
+	const data = await fetch(url);
 	const jsonData = await data.json();
-	console.log(jsonData);
 	return jsonData;
 };
--- src/widgets/SideBar/SideBar.tsx
+++ src/widgets/SideBar/SideBar.tsx
@@ -6,66 +6,66 @@ import AddButton from '../AddButton/AddButton';
 import NoteType from '@/shared/lib/NoteType';
 import { SearchForNotes } from '@/shared/lib/SearchForNotes';
 
-const SideBar = () => {
-	const notesData: NoteType[] = [
-		{
-			noteId: '1',
-			noteTitle: 'Длинное название',
-			noteText: 'Some text in Note',
-			noteDate: '01.01.2023',
-			noteStatus: true,
-		},
-		{
-			noteId: '2',
-			noteTitle: 'Супер длинное название заметки',
-			noteText: 'Some text in Note',
-			noteDate: '17.03.2022',
-			noteStatus: true,
-		},
-		{
-			noteId: '3',
-			noteTitle: 'Title3',
-			noteText: 'Some text in Note',
-			noteDate: '31.12.1981',
-			noteStatus: false,
-		},
-		{
-			noteId: '4',
-			noteTitle: 'Title4',
-			noteText: 'Some text in Note',
-			noteDate: '25.07.1792',
-			noteStatus: true,
-		},
-		{
-			noteId: '5',
-			noteTitle: 'Какой то текст',
-			noteText: 'Some text in Note',
-			noteDate: '25.07.1792',
-			noteStatus: false,
-		},
-		{
-			noteId: '6',
-			noteTitle: 'Заметка',
-			noteText: 'Some text in Note',
-			noteDate: '25.07.1792',
-			noteStatus: true,
-		},
-		{
-			noteId: '7',
-			noteTitle: 'Очередная заметка',
-			noteText: 'Some text in Note',
-			noteDate: '25.07.1792',
-			noteStatus: false,
-		},
-		{
-			noteId: '8',
-			noteTitle: 'Последняя заметка',
-			noteText: 'Some text in Note',
-			noteDate: '25.07.1792',
-			noteStatus: true,
-		},
-	];
+const notesData: NoteType[] = [
+	{
+		noteId: '1',
+		noteTitle: 'Длинное название',
+		noteText: 'Some text in Note',
+		noteDate: '01.01.2023',
+		noteStatus: true,
+	},
+	{
+		noteId: '2',
+		noteTitle: 'Супер длинное название заметки',
+		noteText: 'Some text in Note',
+		noteDate: '17.03.2022',
+		noteStatus: true,
+	},
+	{
+		noteId: '3',
+		noteTitle: 'Title3',
+		noteText: 'Some text in Note',
+		noteDate: '31.12.1981',
+		noteStatus: false,
+	},
+	{
+		noteId: '4',
+		noteTitle: 'Title4',
+		noteText: 'Some text in Note',
+		noteDate: '25.07.1792',
+		noteStatus: true,
+	},
+	{
+		noteId: '5',
+		noteTitle: 'Какой то текст',
+		noteText: 'Some text in Note',
+		noteDate: '25.07.1792',
+		noteStatus: false,
+	},
+	{
+		noteId: '6',
+		noteTitle: 'Заметка',
+		noteText: 'Some text in Note',
+		noteDate: '25.07.1792',
+		noteStatus: true,
+	},
+	{
+		noteId: '7',
+		noteTitle: 'Очередная заметка',
+		noteText: 'Some text in Note',
+		noteDate: '25.07.1792',
+		noteStatus: false,
+	},
+	{
+		noteId: '8',
+		noteTitle: 'Последняя заметка',
+		noteText: 'Some text in Note',
+		noteDate: '25.07.1792',
+		noteStatus: true,
+	},
+];
 
+const SideBar = () => {
 	const [userNotes, setUserNotes] = useState<NoteType[]>([]);
 	const [searchNotes, setSearchNotes] = useState<NoteType[]>([]);
 	const [activeNote, setActiveNote] = useState<number>(-1);
@@ -97,7 +97,7 @@ const SideBar = () => {
 							note={note}
 							active={index === activeNote}
 							openNoteFunction={() => setActiveNote(index)}
-							removeNoteFunction={(event) => {
+							removeNoteFunction={() => {
 								// TASK доделать удаление заметки
 								// Запрос на удаление из БД
 							}}
