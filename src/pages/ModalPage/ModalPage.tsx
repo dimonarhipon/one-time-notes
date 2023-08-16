6f52b7ee64c07e8b2a2109b090510363b37d3f2a
Artur Nakhatakyan <artoim333@gmail.com>
2023-08-16 12:46:15 +0400

    feat: добавлено удаление заметки

--- src/pages/ModalPage/ModalPage.tsx
+++ src/pages/ModalPage/ModalPage.tsx
@@ -6,12 +6,14 @@ import { ModalLink } from '@/widgets/ModalLink/ModalLink';
 import { useEffect, useState } from 'react';
 import { Status } from '@/shared/lib/EnumStatus';
 import { fetchNotes } from '@/shared/api/fetchNotes';
-import styles from './ModalPage.module.scss';
 import { ModalWrapper } from '@/widgets/ModalWrapper/ModalWrapper';
-import Preloader from '@/shared/Preloader/Preloader';
 import { Error404 } from '@/widgets/ModuleError/Error404';
+import { fetchMethods } from '@/shared/api/fetchMethods';
+import Preloader from '@/shared/Preloader/Preloader';
+import styles from './ModalPage.module.scss';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL + 'api/notes/' + location.pathname.substring(13);
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL + 'api/notes/';
+const GET_BACKEND = BACKEND_URL + location.pathname.substring(13);
 
 type GetNote = {
     content: string,
@@ -21,16 +23,25 @@ type GetNote = {
 
 export const ModalPage = () => {
     const [noteState, SetNote] = useState<Status.Open | Status.Get | Status.Delete>(Status.Get);
-    const [fetchState, SetFetch] = useState<GetNote | Status.Error>();
+    const [fetchState, SetFetch] = useState<GetNote | Status.Error | null>();
 
     useEffect(() => {
         (async function () {
-            await fetchNotes(BACKEND_URL).then((note) => {
-                note.name ? SetFetch(Status.Error) :
-                    SetFetch(note);
+            await fetchNotes(GET_BACKEND).then((note) => {
+                switch (note) {
+                    case null:
+                        SetFetch(Status.Error);
+                        break;
+                    default:
+                        note.name ? SetFetch(Status.Error) : SetFetch(note);
+                }
             });
         })();
     }, []);
+
+    const DeleteNote = () => {
+        fetchNotes(BACKEND_URL, fetchMethods.delete);
+    };
 
     const getNote = noteState === Status.Get;
     const openNote = noteState === Status.Open;
@@ -38,7 +49,7 @@ export const ModalPage = () => {
 
     return <ModalTemplate >
         {!fetchState && <Preloader />}
-        {(fetchState === Status.Error) ? <Error404>
+        {(fetchState === (Status.Error || null)) ? <Error404>
             Страница не найдена
         </Error404> : (
             <ModalWrapper active={!!fetchState}>
@@ -46,7 +57,7 @@ export const ModalPage = () => {
                     <ModalTitle noteState={noteState} />
                     {openNote && (
                         <button className={styles.navigationOpen}>
-                            <svg className={styles.navigationImg} viewBox='0 0 1024 1024' onClick={() => SetNote(Status.Delete)}>
+                            <svg className={styles.navigationImg} viewBox='0 0 1024 1024' onClick={() => { SetNote(Status.Delete); DeleteNote(); }}>
                                 <path d='M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z' />
                             </svg>
                         </button>
