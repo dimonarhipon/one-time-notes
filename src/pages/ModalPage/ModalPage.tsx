a6b760db6a8740549b78d9e7645085ff81b54e54
Artur Nakhatakyan <artoim333@gmail.com>
2023-08-16 15:07:43 +0400

    feat: добавлен заголовок сообщения

--- src/pages/ModalPage/ModalPage.tsx
+++ src/pages/ModalPage/ModalPage.tsx
@@ -77,7 +77,7 @@ export const ModalPage = () => {
                 )}
 
                 {openNote && (
-                    <Message>
+                    <Message title={fetchState?.title}>
                         {fetchState?.content}
                     </Message>
                 )}
--- src/widgets/Message/Message.tsx
+++ src/widgets/Message/Message.tsx
@@ -1,8 +1,9 @@
-import { TModalProps } from '@/shared/lib/TModalProps';
 import styles from './Message.module.scss';
 
-export const Message = ({ children }: TModalProps) => {
-	return <div className={styles.messege}>{children}</div>;
+export const Message = ({ children, title }: { children: string | React.ReactNode, title?: string }) => {
+	return <div className={styles.messege}>
+		<div className={styles.messegeTitle}>{title}</div>
+		<div className={styles.messegeContent}>{children}</div>
+	</div>;
 };
 
-//TODO  Доделать отображение абзацей
