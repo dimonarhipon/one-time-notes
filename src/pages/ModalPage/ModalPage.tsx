db129e68b9dba15e309571cde59e939b324b143d
Artur Nakhatakyan <artoim333@gmail.com>
2023-08-19 23:37:10 +0400

    refactor: исправлен обработчик ошибок в запросе, и нейминг типа в компонента

--- src/pages/ModalPage/ModalPage.tsx
+++ src/pages/ModalPage/ModalPage.tsx
@@ -15,24 +15,14 @@ import Preloader from '@/shared/Preloader/Preloader';
 
 export const ModalPage = () => {
     const [noteState, SetNote] = useState<modalMethods.Open | modalMethods.Get | modalMethods.Delete>(modalMethods.Get);
-    const [fetchState, SetFetch] = useState<TGetNote | modalMethods.Error | null>();
+    const [fetchState, SetFetch] = useState<TGetNote | modalMethods.Error>();
 
     useEffect(() => {
-        try{
-            (async function () {
-                await fetchNotes(FULL_PATH).then((note) => {
-                    switch (note) {
-                        case null:
-                            SetFetch(modalMethods.Error);
-                            break;
-                        default:
-                            note.name ? SetFetch(modalMethods.Error) : SetFetch(note);
-                    }
-                });
-            })();
-        } catch {
+        fetchNotes(FULL_PATH).then((note) => {
+            note.name ? SetFetch(modalMethods.Error) : SetFetch(note);
+        }).catch(() => {
             SetFetch(modalMethods.Error);
-        }
+        });
     }, []);
 
     const getNote = noteState === modalMethods.Get;
@@ -48,8 +38,8 @@ export const ModalPage = () => {
                 <div className={styles.navigationСontainer}>
                     <ModalTitle noteState={noteState} />
                     {openNote && (
-                        <button className={styles.navigationOpen}>
-                            <svg className={styles.navigationImg} viewBox='0 0 1024 1024' onClick={() => { SetNote(modalMethods.Delete); DeleteNote(); }}>
+                        <button className={styles.navigationOpen} onClick={() => { SetNote(modalMethods.Delete); DeleteNote(); }}>
+                            <svg className={styles.navigationImg} viewBox='0 0 1024 1024' >
                                 <path d='M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z' />
                             </svg>
                         </button>
--- src/widgets/Message/Message.tsx
+++ src/widgets/Message/Message.tsx
@@ -1,10 +1,10 @@
 import styles from './Message.module.scss';
 
-type MessageProps = {
+type TMessageProps = {
 	children: string | React.ReactNode, title?: string
 };
 
-export const Message = ({ children, title }: MessageProps ) => {
+export const Message = ({ children, title }: TMessageProps ) => {
 	return <div className={styles.messege}>
 		<div className={styles.messegeTitle}>{title}</div>
 		<div className={styles.messegeContent}>{children}</div>
