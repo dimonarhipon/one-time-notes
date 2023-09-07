f3875269b0846033da939b103850863c90c28419
D_arh <dimonarhipon@mail.ru>
2023-09-07 20:46:39 +0300

    perf: удаляет неиспользуемые компоненты иконок

--- src/shared/assets/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { IconTheme } from './IconTheme';
-export { IconLang } from './IconLang';
-export { IconEye } from './IconEye';
-export { IconEyeSlash } from './IconEyeSlash';
-export { SvgIcon } from '../../SvgIcon/SvgIcon';
